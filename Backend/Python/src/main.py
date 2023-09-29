3728dabcb0aeef22154fb34044388f3da7b032ed
Allen Román <2909080001301@ingenieria.usac.edu.gt>
2023-09-29 17:16:48 -0600

    Update Py cors.

--- Backend/Python/src/main.py
+++ Backend/Python/src/main.py
@@ -12,7 +12,7 @@ from controllers.Home import BlueprintHome
 
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"/*":{"origin":"*"}})
 
 
 
@@ -31,4 +31,4 @@ def index():
     return 'Hello World'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=4000)
